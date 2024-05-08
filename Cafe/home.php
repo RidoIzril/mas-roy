f576a9b97e6c1f567b8db6a44d4c92c31ea216e5
RidoIzril <164135241+RidoIzril@users.noreply.github.com>
2024-05-08 21:35:18 +0700

    Add files via upload

---
+++ Cafe/home.php
@@ -0,0 +1,17 @@
+<div class="col-lg-9 mt-3">
+    <div class="card">
+        <div class="card-header">
+            Home
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Ini Bagian Home</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content. Lorem ipsum
+                dolor sit amet consectetur adipisicing elit. Fugiat ullam similique dicta impedit, provident nostrum
+                nemo
+                autem corrupti veniam error porro nobis unde obcaecati repudiandae mollitia, aperiam omnis, quis
+                officiis.
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+</div>
---
+++ Cafe/proses/proses_delete_user.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+include "connect.php";
+$id = (isset($_POST['id'])) ? htmlentities($_POST['id']) : "";
+
+if (!empty($_POST['input_user_validate'])) {
+    $query = mysqli_query($conn, "DELETE FROM tb_user WHERE id ='$id'");
+    if ($query) {
+        $message = '<script>alert("Data berhasil diupdate");
+                    window.location="../user"</script>
+                    </script>';
+    } else {
+        $message = '<script>alert("Data gagal diupdate")</script>';
+
+    }
+}
+echo $message;
+?>
